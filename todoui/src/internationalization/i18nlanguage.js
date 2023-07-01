7be72ab1d49cd44cdc45ebd29ebb465f329db30e
VuranokYigit <fyvuranokk@gmail.com>
2023-07-01 16:50:56 +0300

    commentProject

--- todoui/src/internationalization/i18nlanguage.js
+++ todoui/src/internationalization/i18nlanguage.js
@@ -6,39 +6,14 @@ i18n.use(initReactI18next).init({
         en:
         {
             translations: {
-                'new_todo_input': "Wash the dishes...",
-                'todo_input': "Todo Input",
-                'add_new_task': "Add new task",
-                'all_list': "All",
-                'done_list': "Done",
-                'todo_list': "Todo",
-                'todo_list_header': "Todo List",
-                'delete_all_done': "Delete done tasks",
-                'delete_all_tasks': "Delete all tasks",
-                'footer_info': 'All Rights Reserved.',
-                'footer_desinger': 'Design and codes By Furkan DUMANLI',
-                'update_page': "Update Page",
-                'update_submit': "Update the task",
-                'unexpected_error':'Unexpected Error Occurred',
-            }
+                "en language content"
+                
         },
         tr:
         {
             translations: {
-                'new_todo_input': "Bulaşıkları yıka...",
-                'todo_input': "Yeni Görev",
-                'add_new_task': "Yeni Görev Ekle",
-                'all_list': "Bütün Liste",
-                'done_list': "Yapılanlar",
-                'todo_list': "Yapılacaklar",
-                'todo_list_header': "Yapılacaklar Listesi",
-                'delete_all_done': "Yapılanları Sil",
-                'delete_all_tasks': "Hepsini Sil",
-                'footer_info': 'Bütün Haklar Saklıdır.',
-                'footer_desinger': 'Furkan Dumanlı tarafından Tasarlandı ve Kodlandı',
-                'update_page': "Güncelleme Sayfası",
-                'update_submit': "Görevi Güncelle",
-                'unexpected_error':'Beklenmedik Hata Oluştu',
+                "tr language content"
+                
             }
         }
     },
--- todoui/src/service/todoapiService.js
+++ todoui/src/service/todoapiService.js
@@ -2,43 +2,43 @@
 import axios from "axios"
 
 // PERSISTE URL
-const TODO_URL = "/todos"
+const REACT_URL = "/todos"
 
 // CLASS
 class TodoApiServices {
     // CREATE
-    // localhost:2222/todos/create
+    // localhost:3333/todos/create
     todoServiceCreate(todoDto) {
-        return axios.post(TODO_URL + "/create", todoDto);
+        return axios.post(REACT_URL + "/create", todoDto);
     }
 
     // LIST
-    // localhost:2222/todos/list
+    // localhost:3333/todos/list
     todoServiceList() {
-        return axios.get(TODO_URL+"/list");
+        return axios.get(REACT_URL+"/list");
     }
 
     // FIND
-    // localhost:2222/todos/find/1
+    // localhost:3333/todos/find/1
     todoServiceFindById(id) {
-        return axios.get(`${TODO_URL}/find/${id}`);
+        return axios.get(`${REACT_URL}/find/${id}`);
     }
 
     // DELETE
-    // localhost:2222/todos/delete/1
+    // localhost:3333/todos/delete/1
     todoServiceDeleteById(id) {
-        return axios.delete(`${TODO_URL}/delete/${id}`);
+        return axios.delete(`${REACT_URL}/delete/${id}`);
     }
     // DELETE ALL
-    // localhost:2222/todos/all/delete
+    // localhost:3333/todos/all/delete
     todoServiceDeleteAll() {
-        return axios.get(`${TODO_URL}/all/delete`);
+        return axios.get(`${REACT_URL}/all/delete`);
     }
 
     // UPDATE
-    // localhost:2222/todos/update/1
+    // localhost:3333/todos/update/1
     todoServiceUpdateById(id,todoDto) {
-        return axios.put(`${TODO_URL}/update/${id}`, todoDto);
+        return axios.put(`${REACT_URL}/update/${id}`, todoDto);
     }
 
 }
