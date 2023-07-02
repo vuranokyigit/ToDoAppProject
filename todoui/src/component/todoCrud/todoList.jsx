8cc7b1e302fe8d2ffb0291b75e667ac14b87e8b5
VuranokYigit <fyvuranokk@gmail.com>
2023-07-03 02:23:05 +0300

    todoListUp

--- todoui/src/component/todoCrud/todoList.jsx
+++ todoui/src/component/todoCrud/todoList.jsx
@@ -5,48 +5,59 @@ import { useEffect } from "react";
 
 function TodoList() {
     const [todoList, setTodoList] = useState([]);
-  
+
     useEffect(() => {
-      todoapiService.todoServiceList()
-        .then((response) => {
-          setTodoList(response.data);
-        })
-        .catch((error) => {
-          console.error("List failed", error);
-        });
+        todoapiService.todoServiceList()
+            .then((response) => {
+                setTodoList(response.data);
+            })
+            .catch((error) => {
+                console.error("List failed", error);
+            });
     }, []);
 
-  
-        return (
-            <>
-                <h1 className="text-center text-primary display-5 text-uppercase"><i class="fa-solid fa-circle-plus me-3"></i>create</h1>
-                <table className="table table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>HEADER</th>
-                            <th>CONTENT</th>
-                            <th>DATE</th>
-                            <th>UPDATE</th>
-                            <th>VIEW</th>
-                            <th>DELETE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>HEADER-1</td>
-                            <td>CONTENT-1</td>
-                            <td>DATE-1</td>
-                            <td><i class="fa-solid fa-pen-to-square text-primary"></i></td>
-                            <td><i class="fa-solid fa-binoculars text-dark"></i></td>
-                            <td><i class="fa-solid fa-trash text-danger me-3"></i></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </>
-        )
-    }
+
+    return (
+        <>
+            <h1 className="text-center text-primary display-5 text-uppercase">TO DO TASKs</h1>
+            {/* CREATE TASK */}
+            <button className="btn btn-primary" style={{marginRight:"2rem"}}><i class="fa-solid fa-circle-plus me-3"> ADD task</i></button>
+            {/* ALL DELETE TASK */}
+            <button className="btn btn-danger"><i class="fa-solid fa-bomb">  ALL tasks delete</i></button>
+            <table className="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>HEADER</th>
+                        <th>CONTENT</th>
+                        <th>DATE</th>
+                        <th>UPDATE</th>
+                        <th>VIEW</th>
+                        <th>DELETE</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        todoList.map((todo) =>
+                            <tr key={todo.id}>
+                                <td>{todo.id}</td>
+                                <td>{todo.header}</td>
+                                <td>{todo.content}</td>
+                                <td>{todo.systemDate}</td>
+                                <td><i class="fa-solid fa-pen-to-square text-primary"></i></td>
+                                <td><i class="fa-solid fa-binoculars text-dark"></i></td>
+                                <td><i class="fa-solid fa-trash text-danger me-3"></i></td>
+                            </tr>
+
+                        )//end mapping
+                    }
+
+                </tbody>
+            </table>
+        </>
+    )
+}
 
 
 export default TodoList;
